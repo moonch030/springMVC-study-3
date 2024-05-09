cc317d064830f5c87e8dd1ef5aea55e9068a4f9d
moonch <cogus2010@naver.com>
2024-05-09 16:41:23 +0900

    add: memberController

--- ch07_pjt_02/src/main/java/com/company/hello/member/MemberController.java
+++ ch07_pjt_02/src/main/java/com/company/hello/member/MemberController.java
@@ -1,6 +1,9 @@
 package com.company.hello.member;
 
-import com.company.hello.HomeController;
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -8,27 +11,37 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
 @Controller
 public class MemberController {
-    public static final Logger logger = LoggerFactory.getLogger(MemberController.class);
+	
+	private static final Logger logger = 
+			LoggerFactory.getLogger(MemberController.class);
+	
+	@RequestMapping(value="/" , method=RequestMethod.GET)
+	public String home(Locale locale, Model model) 
+	{
+		System.out.println("MemberController의 home()메서드");
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
+	}
+	
+	
+	@RequestMapping("/signup")
+	public String signup() {
+		return "sign_up";
+	}
+	
+	@RequestMapping("/signin")
+	public String signin() {
+		return "sign_in";
+	}
 
-    @RequestMapping(value = "/member", method = RequestMethod.GET)
-    public String home(Locale locale, Model model) {
-        logger.info("Welcome home! The client locale is {}.", locale);
-
-        System.out.println("MemberController.home");
-
-        Date date = new Date();
-        DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-
-        String formattedDate = dateFormat.format(date);
-
-        model.addAttribute("serverTime", formattedDate );
-
-        return "home";
-    }
 }
--- ch07_pjt_02/src/main/webapp/WEB-INF/views/home.jsp
+++ ch07_pjt_02/src/main/webapp/WEB-INF/views/home.jsp
@@ -7,7 +7,7 @@
 </head>
 <body>
 <h1>
-	안녕하세요
+	안녕하세요 hihi
 </h1>
 
 <P>  The time on the server is ${serverTime}. </P>
