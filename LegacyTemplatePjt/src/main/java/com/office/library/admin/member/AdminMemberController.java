95762258f381526a82a11b8d9a6e510787f78d04
moonch <cogus2010@naver.com>
2024-05-30 15:03:06 +0900

    2024-05-30

--- LegacyTemplatePjt/src/main/java/com/office/library/admin/member/AdminMemberController.java
+++ LegacyTemplatePjt/src/main/java/com/office/library/admin/member/AdminMemberController.java
@@ -1,16 +1,27 @@
 package com.office.library.admin.member;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/admin/member")
 public class AdminMemberController {
 
-	@RequestMapping("/createAccountForm")
+//	@RequestMapping("/createAccountForm")	
+//	@RequestMapping(value="/createAccountForm",method=RequestMethod.GET)
+	@GetMapping("/createAccountForm")
 	public String createAccountForm() {
-		System.out.println("--- AdminMemberController의 createAccountForm메서드 ---");
+		System.out.println("--- AdminMemberController의 createAccountForm() 호출 ---");
 		String nextPage = "admin/member/create_account_form";
+		return nextPage;
+	}
+	
+	@RequestMapping(value="/createAccountConfirm",method=RequestMethod.POST)
+	public String createAccountConfirm() {
+		System.out.println("--- AdminMemberController의 createAccountConfirm() 호출 ---");
+		String nextPage = "admin/member/create_account_ok";
 		return nextPage;
 	}
 }
